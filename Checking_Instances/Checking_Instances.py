9c635f3fdc27408f6270bf3bf66bd3d612cbe9ce
LOKESH A D <aakulalokesh2007@gmail.com>
2026-06-03 20:05:35 +0530

    Add files via upload

---
+++ Checking_Instances/Checking_Instances.py
@@ -0,0 +1,12 @@
+class he:
+    def no(self):
+     pass
+class hr:
+    def show(self):
+        pass
+a=he()
+b=hr()
+print(isinstance(b,he))
+print(isinstance(a,he))
+print(isinstance(b,hr))
+print(isinstance(a,hr))
